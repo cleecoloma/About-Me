0d1acadd3a816747c7efe6a44ea5cddc3e0683e1
Chester Lee Coloma <cleecoloma@gmail.com>
2023-07-11 14:00:18 -0700

    addded javascript code for prompts and alerts

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+let userName = '';
+
+const questions = ['Are dogs capable of recognizing themselves in a mirror?', 'Can dogs understand human emotions?', 'Do dogs have a sense of time?', 'Can dogs see color?', 'Are dogs capable of feeling empathy for other animals?']
+
+const answers = []
+
+const correctAnswers = ['no', 'yes', 'no', 'yes', 'yes']
+
+const explanations = ["Dogs typically do not recognize themselves in a mirror. They may react to their reflection as if it's another dog or show curiosity, but they generally do not understand that it is their own image.", "Dogs typically do not recognize themselves in a mirror. They may react to their reflection as if it's another dog or show curiosity, but they generally do not understand that it is their own image.", "Dogs do not have a precise sense of time like humans do. While they may have an internal sense of routine and be able to anticipate certain events, their perception of time is primarily based on associative memory rather than a linear understanding of hours or minutes.", "Dogs can see colors, but their range of color vision is more limited compared to humans. They primarily see the world in shades of blue and yellow, with difficulty distinguishing between red and green hues.", "Dogs are capable of feeling empathy for other animals, including humans and other dogs. They can show concern, comfort, and exhibit behaviors that indicate empathy towards those in distress or pain. However, the extent of empathy may vary among individual dogs."]
+
+function initialQuestions(questions, answers) {
+  // First, we display message telling the user about the 5 questions
+  userName = prompt('What is your name?');
+  alert('Please answer the following FIVE questions by answering "Yes" or "No"');
+  // Below iterates the questions using a for loop using the length of the string as the condition
+  for (i=0; i < questions.length; i++) {
+    //Below adds the the user's answer to the answers array
+    answers.push(prompt(questions[i]));
+    console.log('The user answered ' + answers[i] + ' for Question #' +{i});
+    //Below checks the user's answer with the correct answer, outputs either correct or wrong, then explains why using the explanations array
+    if (answers[i] === correctAnswer[i]) {
+      alert('Correct!\n' + {explanations[i]});
+    } else {
+      alert('Wrong\n' + {explanations[i]})
+    }
+  }
+  alert('Thank you for playing ' + {userName});
+}
