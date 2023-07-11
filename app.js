7fa2730842f420f1c0c0569b4fa5c0cd947bcd7f
Chester Lee Coloma <cleecoloma@gmail.com>
2023-07-11 14:38:14 -0700

    js code is done. trying to get the if statement to work

--- app.js
+++ app.js
@@ -1,30 +1,60 @@
-'use strict';
+"use strict";
 
-let userName = '';
+let userName = "";
 
-const questions = ['Are dogs capable of recognizing themselves in a mirror?', 'Can dogs understand human emotions?', 'Do dogs have a sense of time?', 'Can dogs see color?', 'Are dogs capable of feeling empathy for other animals?']
+//These questions are taken from ChatGPT
+const questions = [
+  "Are dogs capable of recognizing themselves in a mirror?",
+  "Can dogs understand human emotions?",
+  "Do dogs have a sense of time?",
+  "Can dogs see color?",
+  "Are dogs capable of feeling empathy for other animals?",
+];
 
-const answers = []
+const answers = [];
 
-const correctAnswers = ['no', 'yes', 'no', 'yes', 'yes']
+//These answers are taken from ChatGPT
+const correctAnswers = ['no', 'yes', 'no', 'yes', 'yes'];
 
-const explanations = ["Dogs typically do not recognize themselves in a mirror. They may react to their reflection as if it's another dog or show curiosity, but they generally do not understand that it is their own image.", "Dogs typically do not recognize themselves in a mirror. They may react to their reflection as if it's another dog or show curiosity, but they generally do not understand that it is their own image.", "Dogs do not have a precise sense of time like humans do. While they may have an internal sense of routine and be able to anticipate certain events, their perception of time is primarily based on associative memory rather than a linear understanding of hours or minutes.", "Dogs can see colors, but their range of color vision is more limited compared to humans. They primarily see the world in shades of blue and yellow, with difficulty distinguishing between red and green hues.", "Dogs are capable of feeling empathy for other animals, including humans and other dogs. They can show concern, comfort, and exhibit behaviors that indicate empathy towards those in distress or pain. However, the extent of empathy may vary among individual dogs."]
+//These explanations are taken from ChatGPT
+const explanations = [
+  "Dogs typically do not recognize themselves in a mirror. They may react to their reflection as if it's another dog or show curiosity, but they generally do not understand that it is their own image.",
+  "Dogs typically do not recognize themselves in a mirror. They may react to their reflection as if it's another dog or show curiosity, but they generally do not understand that it is their own image.",
+  "Dogs do not have a precise sense of time like humans do. While they may have an internal sense of routine and be able to anticipate certain events, their perception of time is primarily based on associative memory rather than a linear understanding of hours or minutes.",
+  "Dogs can see colors, but their range of color vision is more limited compared to humans. They primarily see the world in shades of blue and yellow, with difficulty distinguishing between red and green hues.",
+  "Dogs are capable of feeling empathy for other animals, including humans and other dogs. They can show concern, comfort, and exhibit behaviors that indicate empathy towards those in distress or pain. However, the extent of empathy may vary among individual dogs.",
+];
 
-function initialQuestions(questions, answers) {
+function initialQuestions() {
   // First, we display message telling the user about the 5 questions
-  userName = prompt('What is your name?');
-  alert('Please answer the following FIVE questions by answering "Yes" or "No"');
+  userName = prompt("What is your name?");
+  alert(
+    'Please answer the following FIVE questions by answering\n"Yes" or "No"'
+  );
   // Below iterates the questions using a for loop using the length of the string as the condition
-  for (i=0; i < questions.length; i++) {
+  for (let i = 0; i < questions.length; i++) {
     //Below adds the the user's answer to the answers array
     answers.push(prompt(questions[i]));
-    console.log('The user answered ' + answers[i] + ' for Question #' +{i});
+    let j = i + 1;
+    console.log("The user answered " + answers[i] + " for Question #" + j);
     //Below checks the user's answer with the correct answer, outputs either correct or wrong, then explains why using the explanations array
-    if (answers[i] === correctAnswer[i]) {
-      alert('Correct!\n' + {explanations[i]});
+    if (answers[i].toLowerCase === correctAnswers[i]) {
+      alert(
+        "Correct!\n\nThe answer is " +
+          correctAnswers[i] +
+          "\n\nSee below for explanation\n" +
+          explanations[i]
+      );
     } else {
-      alert('Wrong\n' + {explanations[i]})
+      alert(
+        "Wrong\n\nThe correct answer is " +
+          correctAnswers[i] +
+          "\n\nSee below for explanation\n" +
+          explanations[i]
+      );
     }
   }
-  alert('Thank you for playing ' + {userName});
+  alert("Thank you for playing " + userName);
 }
+
+initialQuestions();
